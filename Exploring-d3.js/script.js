5b09150a7919903fff47eae259e304e7b1bfee74
jlsears <jen.lauren.sears@gmail.com>
2016-11-01 19:52:32 -0500

    more playing with appending

--- Exploring-d3.js/script.js
+++ Exploring-d3.js/script.js
@@ -3,9 +3,20 @@ svg=d3.select("#chart")
 .append("svg")
 .attr("width",w)
 .attr("height",h);
+
+var svg2 = d3.select("#chart")
+.append("svg")
+.attr("width", w)
+.attr("height", h);
  
 var text=svg
 .append("text")
 .text("hello world")
-.attr("y",250)
+.attr("y", 250)
+.attr("x", 600);
+
+var moreText = svg2
+.append("text")
+.text("I said 'ello'")
+.attr("y", 500)
 .attr("x", 600);

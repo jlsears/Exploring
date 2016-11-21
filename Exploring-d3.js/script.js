61734d63b2ab7d7a3b0011dfe24f50ce2b5ba04d
jlsears <jen.lauren.sears@gmail.com>
2016-11-20 21:54:34 -0600

    relocated #chart2 style from css file to js file

--- Exploring-d3.js/script.js
+++ Exploring-d3.js/script.js
@@ -1,5 +1,5 @@
 var w=960,h=500,
-svg=d3.select("#chart")
+svg1=d3.select("#chart")
 .append("svg")
 .attr("width",w)
 .attr("height",h)
@@ -9,9 +9,15 @@ var svg2 = d3.select("#chart2")
 .append("svg")
 .attr("width", w)
 .attr("height", h)
-.attr("class", "chart2");;
+.attr("class", "chart2")
+.style("background-color", "darkseagreen")
+.style("fill", "white")
+.style("border", "5px solid")
+.style("border-color", "peru")
+.style("font-family", "arial")
+.style("font-size", "25px");
  
-var text=svg
+var text=svg1
 .append("text")
 .text("hello world")
 .attr("y", 250)
@@ -22,3 +28,8 @@ var moreText = svg2
 .text("I said 'ello'")
 .attr("y", 250)
 .attr("x", 600);
+
+var fancierCircle = d3.select("#fancyCircle");
+	fancierCircle.attr("r", 50);
+	fancierCircle.style("stroke", "darkseagreen");
+	fancierCircle.style("fill", "orange");

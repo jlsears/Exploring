9c36e97737b43c3e52057fcf85969abd3e265946
jlsears <jen.lauren.sears@gmail.com>
2016-12-08 19:26:13 -0600

    beginning line graph, drawing x and y axis

---
+++ Exploring-d3.js/lineGraph.js
@@ -0,0 +1,36 @@
+
+var data2 = [1, 3, 4, 3, 6, 1, 8, 2, 4, 1, 3, 4, 1];
+var data1 = [3, 6, 2, 7, 5, 2, 1, 3, 8, 9, 2, 5, 7];
+
+var w = 500;
+var h = 200;
+var margin = 20;
+
+var y = d3.scale.linear().domain([0, d3.max(data1)]).range([0 + margin, h - margin]);
+var x = d3.scale.linear().domain([0, data1.length]).range([0 + margin, w - margin]);
+
+var vis = d3.select("#viz")
+	.append("svg:svg")
+	.attr("width", w)
+	.attr("height", h);
+
+var g = vis.append("svg:g")
+	.attr("transform", "translate(0, 200)");
+
+g.append("svg:line")
+	.transition()
+	.duration(1000)
+	.style("stroke", "black")
+	.attr("x1", x(0))
+	.attr("y1", -1 * y(0))
+	.attr("x2", x(w))
+	.attr("y2", -1 * y(0));
+
+g.append("svg:line")
+	.transition()
+	.duration(1000)
+	.style("stroke", "black")
+	.attr("x1", x(0))
+	.attr("y1", -1 * y(0))
+	.attr("x2", x(0))
+	.attr("y2", -1 * y(d3.max(data1)));

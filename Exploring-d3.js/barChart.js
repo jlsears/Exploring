78538c33fbff943249645416964a75ec57941242
jlsears <jen.lauren.sears@gmail.com>
2016-12-10 15:29:53 -0600

    commenting

--- Exploring-d3.js/barChart.js
+++ Exploring-d3.js/barChart.js
@@ -1,4 +1,8 @@
 
+// *********************************
+// INITIAL GOLD RECTANGE
+// *********************************
+
 var rectDemo = d3.select("#rect1")
 	.append("svg:svg")
 	.attr("width", 400)
@@ -12,25 +16,40 @@ rectDemo.append("svg:rect")
 	.style("fill", "gold");
 
 
-  var data = [{year: 2006, deaths: 55},
-        {year: 2007, deaths: 63},
-        {year: 2008, deaths: 69},
-        {year: 2009, deaths: 81},
-        {year: 2010, deaths: 74},
-        {year: 2011, deaths: 79},
-        {year: 2012, deaths: 93}];
+// *********************************
+// BAR CHART
+// *********************************
+
+// ***VALUES***
+
+var data = [{year: 2006, deaths: 55},
+  {year: 2007, deaths: 63},
+  {year: 2008, deaths: 69},
+  {year: 2009, deaths: 81},
+  {year: 2010, deaths: 74},
+  {year: 2011, deaths: 79},
+  {year: 2012, deaths: 93}];
 
 var barWidth = 40;
 var width = (barWidth + 10) * data.length;
 var height = 200;
 
+
+// ***FOR CREATING COORDINATES***
+
 var x = d3.scale.linear().domain([0, data.length]).range([0, width]);
 var y = d3.scale.linear().domain([0, d3.max(data, function(data){ return data.deaths})]).rangeRound([0, height]);
+
+
+// ***SETTING UP CONTAINER/CANVAS***
 
 var barBasic = d3.select("#bar-basic")
 	.append("svg:svg")
 	.attr("width", width)
 	.attr("height", height);
+
+
+// ***CREATING/APPENDING BARS***
 
 barBasic.selectAll("rect")
 	.data(data)
@@ -43,6 +62,9 @@ barBasic.selectAll("rect")
 	.attr("fill", "purple");
 
 var xScale = d3.scale.linear().domain([0, 20]).range(([0, 100]));
+
+
+// ***ADDING TEXT FOR DATA POINTS TO BARS***
 
 barBasic.selectAll("text")
 	.data(data)

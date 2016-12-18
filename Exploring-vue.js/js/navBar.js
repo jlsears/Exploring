32f5e0e4caafba64a6250be06088f5d3988361f9
jlsears <jen.lauren.sears@gmail.com>
2016-12-18 17:53:21 -0600

    commenting

--- Exploring-vue.js/js/navBar.js
+++ Exploring-vue.js/js/navBar.js
@@ -1,7 +1,11 @@
 
+//Vue instance binding the model and view together
 var demo = new Vue({
 
+	// DOM element on which to mount view model
 	el: '#main',
+
+	// The model
 	data: {
 		active: 'home'
 	},

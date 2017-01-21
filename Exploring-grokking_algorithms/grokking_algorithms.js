f84a562ed2e5ae170e1cd82a2e5e0785a3f6a998
jlsears <jen.lauren.sears@gmail.com>
2017-01-20 23:23:52 -0600

    implementing quicksort

--- Exploring-grokking_algorithms/grokking_algorithms.js
+++ Exploring-grokking_algorithms/grokking_algorithms.js
@@ -18,3 +18,35 @@ var tryRecursion = function(x) {
 };
 
 var goRecursion = tryRecursion(startHere);
+
+
+// Chapter 4: Quicksort
+
+
+var handyArray = [8, 48, 6, 9, 24, 32, 3, 4];
+
+var counter = 0;
+
+var quicklyNow = function(sortThis) {
+
+	counter = counter + 1;
+
+	if(sortThis.length < 2) {
+		return sortThis;
+	}
+	else{
+		var less = [];
+		var greater = [];
+		var findPivot = [sortThis[((sortThis.length/2)-1).toFixed()]];
+	
+		sortThis.forEach(function(item) {
+			if(item < findPivot[0]){ 
+				less.push(item);
+			}
+			else{if(item > findPivot[0]){ 
+				greater.push(item);
+			}}
+		})
+		return nextArray = quicklyNow(less).concat(findPivot, quicklyNow(greater));
+	}
+}
